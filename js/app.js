e29529f84d583190cbac5aab628434d6f5a3db6f
Denise Erazmus <denise.erazmus@gmail.com>
2018-05-11 08:51:24 -0500

    started javascript logic

--- js/app.js
+++ js/app.js
@@ -4,5 +4,12 @@ function submitAnswers(){
 
 
 	//get user input
-	var q1 = 
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+
+	//validation
+	
 }
